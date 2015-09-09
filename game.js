7dc5f6da9efc373dd585fe8a229d3d7380c16362
James Stewart <james.stewart@domo.com>
2015-09-08 19:43:57 -0600

    Working

--- game.js
+++ game.js
@@ -1,14 +1,77 @@
 var game = (function() {
-	var bowl = function(pins) {
-		
+
+  const maxPins = 10;
+  const maxFrames = 10;
+
+  var currentFrame;
+  var currentBall;
+  var currentPins;
+  var ballsToAdd;
+  var frames;
+  var strikes;
+  var spares;
+  var ended;
+
+  init();
+
+  function init() {
+    currentFrame = 0;
+    currentBall = 0;
+    currentPins = maxPins;
+    ballsToAdd = 0;
+    frames = [[]];
+    strikes = [];
+    spare = false;
+    ended = false;
+  }
+
+  function bowl(pins) {
+    strikes = strikes.filter(strike => {
+      frames[strike.frame][0] += pins;
+      return strike.balls--;
+    });
+    if (spare) {
+      spare = false;
+      frames[currentFrame-1][1] += pins;
+    }
+    if (!ended){
+      currentPins -= pins;
+      frames[currentFrame][currentBall] = pins;
+      if(!currentPins) {
+        if (currentBall){
+          spare = true;
+        } else {
+          strikes.push({frame: currentFrame, balls:1});
+        }
+        currentBall = 1;
+      }
+      if(currentBall){
+        currentBall = 0;
+        currentPins = maxPins;
+        currentFrame++;
+        if (currentFrame >= maxFrames) {
+          ended = true;
+        } else {
+          frames.push([]);
+        }
+      } else {
+        currentBall++;
+      }
+    }
+  }
+
+	function getScore() {
+    return frames.reduce((score, frame) => frame.reduce((frameScore, ball) => frameScore + ball, 0) + score, 0);
 	}
 
-	var getScore = function(pins) {
-	}
+  function reset() {
+    init();
+  }
 
 	return {
 		bowl: bowl,
-		getScore: getScore
+		getScore: getScore,
+    reset: reset
 	};
 })();
 
--- test/test.js
+++ test/test.js
@@ -4,12 +4,92 @@ var expect = chai.expect;
 var Game = require( "../game.js").game;
 
 describe('Game', function() {
+
+  beforeEach(Game.reset);
+
 	it('should exist', function() {
 		expect(Game).to.be.an.object;
 	});
 
-	it('should be able to score normal points', function() {
-		Game.bowl(2)
-		expect(Game.getScore()).to.equal(2);
+  it('should be able to score normal points', function() {
+    Game.bowl(2);
+    expect(Game.getScore()).to.equal(2);
+    Game.bowl(6);
+    expect(Game.getScore()).to.equal(8);
+  });
+
+  it('should be able to score strikes', function() {
+    Game.bowl(10);
+    Game.bowl(6);
+    Game.bowl(2);
+    expect(Game.getScore()).to.equal(26);
+  });
+
+	it('should be able to score spares', function() {
+    Game.bowl(5);
+    Game.bowl(5);
+    Game.bowl(2);
+		expect(Game.getScore()).to.equal(14);
 	});
+
+  it('should be able to score 2 strikes in a row', function() {
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(2);
+    expect(Game.getScore()).to.equal(36);
+  });
+
+  it('should be able to score a strike and a spare in a row', function() {
+    Game.bowl(10);
+    Game.bowl(5);
+    Game.bowl(5);
+    Game.bowl(5);
+    expect(Game.getScore()).to.equal(40);
+  });
+
+  it('should be able to score 3 strikes in a row', function() {
+    Game.bowl(10); //30
+    Game.bowl(10); //25
+    Game.bowl(10); //19
+    Game.bowl(5);
+    Game.bowl(4);
+    expect(Game.getScore()).to.equal(83);
+  });
+
+  it('should be able to score a perfect game', function() {
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    Game.bowl(10);
+    expect(Game.getScore()).to.equal(300);
+  });
+
+  it('should be able to score a Dutch 200', function() {
+    Game.bowl(10);
+    Game.bowl(9);
+    Game.bowl(1);
+    Game.bowl(10);
+    Game.bowl(9);
+    Game.bowl(1);
+    Game.bowl(10);
+    Game.bowl(9);
+    Game.bowl(1);
+    Game.bowl(10);
+    Game.bowl(9);
+    Game.bowl(1);
+    Game.bowl(10);
+    Game.bowl(9);
+    Game.bowl(1);
+    Game.bowl(10);
+    expect(Game.getScore()).to.equal(200);
+  });
+
 });
